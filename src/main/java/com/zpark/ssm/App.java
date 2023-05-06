22a49f7047a8442fb65ea253ca19741d39d91da5
gxk <gxk@163.com>
2023-05-06 09:12:07 +0800

    gxk master second commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -11,5 +11,7 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args){
         System.out.println("Hello Git!");
+        System.out.println("Hello Git! gxk master second commit!");
+        System.out.println("Hello Git! gxk hotfix first commit");
     }
 }
