94b07e804476d86651f06f746b12e8342a8e0988
gxk <gxk@163.com>
2023-05-06 09:32:47 +0800

    gxk master first modify!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -12,7 +12,7 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello Git!");
         System.out.println("Hello Git! gxk master second commit!");
-        System.out.println("Hello Git! gxk hotfix first commit");
         System.out.println("Hello Git! gxk hotfix first commit!");
+        System.out.println("Hello Git! gxk master first modify!");
     }
 }
